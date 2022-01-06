5bf66c4a2525db8d8130a5e04b66c38d8e604054
Invictus Maneo <57263117+bhoomikars@users.noreply.github.com>
2022-01-06 10:08:15 +0530

    Create maxProfitSum.java

---
+++ maxProfitSum.java
@@ -0,0 +1,17 @@
+// maxProfit total sum u need to return
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int max_profit = 0;
+     for(int i=1;i<prices.length;i++)
+     {
+         if(prices[i]>prices[i-1])
+         {
+             max_profit=max_profit+prices[i]-prices[i-1];
+         }
+     }
+        return max_profit;
+        
+    }
+}
