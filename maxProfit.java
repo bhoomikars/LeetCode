e5e532330b8978469d1a979200576139e3c5de4c
Invictus Maneo <57263117+bhoomikars@users.noreply.github.com>
2022-01-06 09:55:49 +0530

    Create maxProfit.java

---
+++ maxProfit.java
@@ -0,0 +1,24 @@
+// find the min value in the given array find the max value from that
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int max_profit=0;
+        int min_val = Integer.MAX_VALUE;
+        
+        for(int i=0;i<prices.length;i++)
+        {
+            if(prices[i]<min_val)
+            {
+                min_val=prices[i];
+            }
+            if(prices[i]-min_val>max_profit)
+            {
+                max_profit=prices[i]-min_val;
+            }
+        }
+        return max_profit;
+        
+    }
+}
