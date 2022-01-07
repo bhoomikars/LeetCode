f27a254285a2a94588dc1eb3ff24c1fab2475fa6
Invictus Maneo <57263117+bhoomikars@users.noreply.github.com>
2022-01-07 13:24:16 +0530

    Create findMin.java

---
+++ findMin.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int findMin(int[] nums) {
+        
+        int min = 0;
+        
+        int low = 0;
+        int high = nums.length-1;
+        
+        while(low<=high)
+        {
+        int mid = low+(high-low)/2;
+            if(mid>0 && nums[mid]<nums[mid-1])
+            {
+                return nums[mid];
+            }
+          if(nums[low]<nums[mid] && nums[mid]>nums[high])
+            {
+                low = mid+1;
+            }
+         if(nums[low]>nums[mid] && nums[mid]<nums[high])
+            {
+                high = mid -1;
+            }
+        
+       
+        
+    }
+        return nums[low];
+}
+}
