094b6abd45e69b73fd55c6896053e9329af27215
Invictus Maneo <57263117+bhoomikars@users.noreply.github.com>
2022-01-22 13:03:27 +0530

    Create main.java

---
+++ Threads/UsingThread/main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main (String[] args)
+    {
+        T1 t1 = new T1();
+        T2 t2 = new T2();
+        t1.start();
+        t2.start();
+
+
+    }
+}
