119a6a60b7db6fe56b2e3563f4151650b69357fa
Invictus Maneo <57263117+bhoomikars@users.noreply.github.com>
2022-01-22 13:05:38 +0530

    Create Main.java

---
+++ Threads/Runnable/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main (String[] args)
+    {
+        T1 t1 = new T1();
+        T2 t2 = new T2();
+        Thread thread1 = new Thread(t1);
+        Thread thread2 = new Thread(t2);
+        thread1.start();
+        thread2.start();
+    }
+}
